f290ec5c84a6ec9137d323b43b5d8e40c55b6af4
zedr0n <zedr0nre@gmail.com>
2019-04-07 01:52:02 +0100

    fix tests

--- ZES.Tests/InfraTests.cs
+++ ZES.Tests/InfraTests.cs
@@ -164,7 +164,6 @@ namespace ZES.Tests
             Assert.NotEqual(0, createdAt); 
             
             var statsQuery = new StatsQuery();
-            await RetryUntil(async () => await bus.QueryAsync(statsQuery) == numRoots);
             Assert.Equal(numRoots,bus.Query(statsQuery));
         }
 

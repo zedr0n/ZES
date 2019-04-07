c20fa771e499e78b2143e12cfa23a4dcb97f0391
zedr0n <zedr0nre@gmail.com>
2019-04-07 14:02:31 +0100

    test package

--- ZES.Infrastructure/Projections/HistoricalProjection.cs
+++ ZES.Infrastructure/Projections/HistoricalProjection.cs
@@ -12,7 +12,6 @@ namespace ZES.Infrastructure.Projections
         public HistoricalProjection(IEventStore<IAggregate> eventStore, ILog logger, IMessageQueue messageQueue, ITimeline timeline, T projection) 
             : base(eventStore, logger, messageQueue, timeline)
         {
-            State = new TState();
             foreach (var h in projection.Handlers)
                 Register(h.Key, (e,s) => Timeline.Now <= _timestamp ? h.Value(e,s) : s);
         }

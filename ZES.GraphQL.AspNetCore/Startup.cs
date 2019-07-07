50a9918683d03d324188bada373426972b9e6e80
zedr0n <zedr0nre@gmail.com>
2019-07-07 13:04:00 +0100

    naming cleanup

--- ZES.GraphQL.AspNetCore/Startup.cs
+++ ZES.GraphQL.AspNetCore/Startup.cs
@@ -1,5 +1,4 @@
-﻿using HotChocolate;
-using HotChocolate.AspNetCore;
+﻿using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,7 +12,7 @@ namespace ZES.GraphQL.AspNetCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.WireGraphQl(typeof(Config));
+            services.UseGraphQl(typeof(Config));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- ZES.GraphQL/GraphQlExtensions.cs
+++ ZES.GraphQL/GraphQlExtensions.cs
@@ -2,11 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using HotChocolate;
-using HotChocolate.Execution;
-using HotChocolate.Subscriptions;
 using Microsoft.Extensions.DependencyInjection;
-using NLog;
 using SimpleInjector;
 using SimpleInjector.Lifestyles;
 using ZES.Infrastructure.Attributes;
@@ -23,12 +19,12 @@ namespace ZES.GraphQL
         /// </summary>
         /// <param name="services"><see cref="IServiceCollection"/></param>
         /// <param name="config">Config type containing the domain registration, root queries and mutations</param>
-        public static void WireGraphQl(this IServiceCollection services, Type config)
+        public static void UseGraphQl(this IServiceCollection services, Type config)
         {
             var container = new Container();
             container.Options.DefaultLifestyle = Lifestyle.Singleton;
 
-            WireGraphQl(services, container, new[] { config });
+            UseGraphQl(services, container, new[] { config });
         }
 
         /// <summary>
@@ -37,11 +33,11 @@ namespace ZES.GraphQL
         /// <param name="services"><see cref="IServiceCollection"/></param>
         /// <param name="container"><see cref="SimpleInjector"/> container</param>
         /// <param name="configs">Config types containing the domain registration, root queries and mutations</param>
-        public static void WireGraphQl(this IServiceCollection services, Container container, IEnumerable<Type> configs)
+        private static void UseGraphQl(this IServiceCollection services, Container container, IEnumerable<Type> configs)
         {
             container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
             new CompositionRoot().ComposeApplication(container);
-            container.Register<IServiceCollection>(() => services, Lifestyle.Singleton);
+            container.Register(() => services, Lifestyle.Singleton);
             container.Register<ISchemaProvider, SchemaProvider>(Lifestyle.Singleton);
 
             var config = Logging.NLog.Configure();
@@ -56,9 +52,6 @@ namespace ZES.GraphQL
             }
             
             container.Verify();
-            
-            var schemaProvider = container.GetInstance<ISchemaProvider>();
-            // schemaProvider.Services();
         }
     }
 }

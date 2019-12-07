509a5a32169709c703810602f23fce2a48a8ebb3
zedr0n <zedr0nre@gmail.com>
2019-12-07 10:29:43 +0000

    fix tests

--- ZES.Tests.Domain/Commands/AddRecord.cs
+++ ZES.Tests.Domain/Commands/AddRecord.cs
@@ -1,16 +1,18 @@
+using ZES.Infrastructure.Attributes;
 using ZES.Infrastructure.Domain;
-using ZES.Interfaces.Domain;
 
 namespace ZES.Tests.Domain.Commands
 {
+    [Idempotent]
     public class AddRecord : Command   
     {
         public AddRecord() { }
         public AddRecord(string target, double recordValue) 
+            : base(target)
         {
             RecordValue = recordValue;
         }
         
-        public double RecordValue { get; private set; } 
+        public double RecordValue { get; } 
      }
 }

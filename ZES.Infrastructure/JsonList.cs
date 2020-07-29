be40a3099122fb1e7def9eb3d44880a2689908e8
zedr0n <zedr0n@protonmail.com>
2020-07-29 19:39:04 +0100

    add deserialization support for json arrays

---
+++ ZES.Infrastructure/JsonList.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ZES.Infrastructure
+{
+    /// <inheritdoc />
+    [JsonArray]
+    public class JsonList<T> : ICollection<T>
+    {
+        private readonly List<T> _list = new List<T>();
+
+        /// <inheritdoc />
+        public int Count => _list.Count;
+
+        /// <inheritdoc />
+        public bool IsReadOnly => false;
+
+        /// <inheritdoc />
+        public IEnumerator<T> GetEnumerator() => _list.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <inheritdoc />
+        public void Add(T item) => _list.Add(item);
+
+        /// <inheritdoc />
+        public void Clear() => _list.Clear();
+
+        /// <inheritdoc />
+        public bool Contains(T item) => _list.Contains(item);
+
+        /// <inheritdoc />
+        public void CopyTo(T[] array, int arrayIndex) => _list.CopyTo(array, arrayIndex);
+
+        /// <inheritdoc />
+        public bool Remove(T item) => _list.Remove(item);
+
+    }
+}

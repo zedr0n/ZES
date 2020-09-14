9af4d6f1a10b888c245854c9336d9104dcf80f9a
zedr0n <zedr0n@protonmail.com>
2020-09-14 10:36:11 +0100

    add action state cache to base implementation

--- ZES.Infrastructure/Stochastics/MarkovActionBase.cs
+++ ZES.Infrastructure/Stochastics/MarkovActionBase.cs
@@ -1,3 +1,5 @@
+#define USE_ACTION_STATE_CACHE
+
 using System.Collections.Generic;
 using System.Linq;
 using ZES.Interfaces.Stochastic;
@@ -8,10 +10,32 @@ namespace ZES.Infrastructure.Stochastics
     public abstract class MarkovActionBase<TState> : IMarkovAction<TState>
         where TState : IMarkovState
     {
+        private Dictionary<TState, TState[]> NextStates { get; } = new Dictionary<TState, TState[]>();
+        
         /// <inheritdoc />
-        public abstract IEnumerable<TState> this[TState current] { get; }
+        public virtual IEnumerable<TState> this[TState current]
+        {
+            get
+            {
+            #if USE_ACTION_STATE_CACHE
+                if (!NextStates.ContainsKey(current))
+                    NextStates[current] = GetStates(current);
+
+                return NextStates[current];
+            #else
+                return GetStates(current);
+            #endif
+            }
+        }
 
         /// <inheritdoc />
         public virtual double this[TState from, TState to] => this[from].Contains(to) ? 1.0 : 0.0;
+        
+        /// <summary>
+        /// Generate the possible states using current action
+        /// </summary>
+        /// <param name="current">Current state</param>
+        /// <returns>Possible states</returns>
+        protected abstract TState[] GetStates(TState current);
     }
 }

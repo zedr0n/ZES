bb106b84a4b43493bdbd381a1a05c08107db304f
zedr0n <zedr0n@protonmail.com>
2020-12-06 11:36:25 +0000

    fix aggregate root interfaces

--- ZES.Infrastructure/Domain/AggregateRoot.cs
+++ ZES.Infrastructure/Domain/AggregateRoot.cs
@@ -1,12 +1,13 @@
 using System;
 using ZES.Interfaces;
+using ZES.Interfaces.Domain;
 
 namespace ZES.Infrastructure.Domain
 {
     /// <summary>
     /// Aggregate root base class
     /// </summary>
-    public class AggregateRoot : EventSourced
+    public class AggregateRoot : EventSourced, IAggregate
     {
         /// <inheritdoc />
         protected override void Register<TEvent>(Action<TEvent> action)

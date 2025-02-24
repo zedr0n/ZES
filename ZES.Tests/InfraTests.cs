2e7894539bdcffe9c63a30cb126521fb2d8052d1
zedr0n <zedr0n@protonmail.com>
2025-02-24 14:27:40 +0000

    remove dead code

--- ZES.Tests/InfraTests.cs
+++ ZES.Tests/InfraTests.cs
@@ -219,15 +219,15 @@ namespace ZES.Tests
 
             var id = $"{nameof(CanUpdateRoot)}-Root";
             var command = new CreateRoot(id); 
-            await await bus.CommandAsync(command);
+            await bus.CommandAsync(command);
 
-            var createdAt = (await bus.QueryUntil(new RootInfoQuery(id), r => r.CreatedAt != default)).CreatedAt;
+            //var createdAt = (await bus.QueryUntil(new RootInfoQuery(id), r => r.CreatedAt != default)).CreatedAt;
             
             var updateCommand = new UpdateRoot(id);
-            await await bus.CommandAsync(updateCommand);
+            await bus.CommandAsync(updateCommand);
 
-            await bus.IsTrue(new RootInfoQuery(id), r => r.UpdatedAt > createdAt);
-
+            //await bus.IsTrue(new RootInfoQuery(id), r => r.UpdatedAt > createdAt);
+            await bus.QueryUntil(new RootInfoQuery(id), c => c?.UpdatedAt != default);
             var root = await repo.Find<Root>(id);
             
             var graph = container.GetInstance<IGraph>();
--- ZES/MessageQueue.cs
+++ ZES/MessageQueue.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Threading.Tasks;
 using SimpleInjector;
 using ZES.Infrastructure.Domain;
-using ZES.Infrastructure.Utils;
 using ZES.Interfaces;
 using ZES.Interfaces.Domain;
 using ZES.Interfaces.EventStore;
@@ -23,8 +21,6 @@ namespace ZES
         private readonly ITimeline _timeline;
         private readonly Subject<IEvent> _messages = new();
         private readonly Subject<IAlert> _alerts = new();
-        private readonly ConcurrentDictionary<string, UncompletedMessagesSingleHolder> _messagesHolderDict;
-        private readonly RetroactiveIdHolder _retroactiveIdHolder;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MessageQueue"/> class.
@@ -35,37 +31,13 @@ namespace ZES
         {
             _log = log;
             _timeline = timeline;
-            _messagesHolderDict = new ConcurrentDictionary<string, UncompletedMessagesSingleHolder>();
-            _retroactiveIdHolder = new RetroactiveIdHolder();
-            // RetroactiveExecution.DistinctUntilChanged()
-            //    .Throttle(x => Observable.Timer(TimeSpan.FromMilliseconds(x ? 0 : 10)))
-            //    .StartWith(false)
-            //    .DistinctUntilChanged()
-            //    .Subscribe(x => log.Info($"Retroactive execution : {x}"));
-             
-            // var combinedStates = _commandStateHolders.Select(x => x.Value.CommandState().Select(s => new { x.Key, s }) ).Merge();
-
         }
-        
-        /// <inheritdoc />
-        public IObservable<int> UncompletedMessages =>
-            _messagesHolderDict.GetOrAdd(_timeline.Id, s => new UncompletedMessagesSingleHolder())
-                .UncompletedMessages();
-
-        /// <inheritdoc />
-        public IObservable<bool> RetroactiveExecution =>
-            _retroactiveIdHolder.RetroactiveExecution();
-            // _messagesHolderDict.GetOrAdd(_timeline.Id, s => new UncompletedMessagesSingleHolder()).RetroactiveExecution();
 
         /// <inheritdoc />
         public IObservable<IEvent> Messages => _messages.AsObservable();
 
         /// <inheritdoc />
         public IObservable<IAlert> Alerts => _alerts.AsObservable();
-
-        /// <inheritdoc />
-        public IObservable<int> UncompletedMessagesOnBranch(string branchId) =>
-            _messagesHolderDict.GetOrAdd(branchId, s => new UncompletedMessagesSingleHolder()).UncompletedMessages();
 
         /// <inheritdoc />
         public void Alert(IAlert alert)
@@ -79,44 +51,6 @@ namespace ZES
         {
             _log.Trace(e.GetType().GetFriendlyName(), this);
             _messages.OnNext(e);
-        }
-
-        /// <inheritdoc />
-        public async Task CompleteMessage(IMessage message)
-        {
-            if (message?.Timeline == null)
-                return;
-            
-            var messagesHolder =
-                _messagesHolderDict.GetOrAdd(message.Timeline, s => new UncompletedMessagesSingleHolder());
-            await messagesHolder.UpdateState(b =>
-            {
-                b.Count--;
-                if (b.Count < 0)
-                    throw new InvalidOperationException($"Message {message.Timeline}:{message.GetType()} completed before being produced");
-
-                _log.Debug($"Uncompleted messages : {b.Count}, removed {message.Timeline}:{message.GetType().GetFriendlyName()}");
-
-                return b;
-            }).ConfigureScheduler(TaskScheduler.Default);
-        }
-
-        /// <inheritdoc />
-        public async Task UncompleteMessage(IMessage message)
-        {
-            if (message?.Timeline == null)
-                return;
-            
-            var messagesHolder =
-                _messagesHolderDict.GetOrAdd(message.Timeline, s => new UncompletedMessagesSingleHolder());
-            await messagesHolder.UpdateState(b =>
-            {
-                b.Timeline = message.Timeline;
-                b.Count++;
-                _log.Debug($"Uncompleted messages : {b.Count}, added {message.Timeline}:{message.GetType().GetFriendlyName()}");
-
-                return b;
-            }).ConfigureScheduler(TaskScheduler.Default);
         }
     }
 }

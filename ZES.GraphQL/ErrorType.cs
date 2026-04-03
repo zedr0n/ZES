ee7b2bd1a027518b5e41d020ac0d67721327afd9
zedr0n <zedr0n@protonmail.com>
2026-04-04 00:02:19 +0100

    fix commit: add ErrorType

---
+++ ZES.GraphQL/ErrorType.cs
@@ -0,0 +1,21 @@
+using HotChocolate.Types;
+using JetBrains.Annotations;
+using ZES.Interfaces;
+
+namespace ZES.GraphQL
+{
+    /// <inheritdoc cref="ObjectType" />
+    [UsedImplicitly]
+    public class ErrorType : ObjectType<IError>
+    {
+        /// <inheritdoc cref="ObjectType.Configure(IObjectTypeDescriptor)" />
+        protected override void Configure(IObjectTypeDescriptor<IError> descriptor)
+        {
+            descriptor.Field(t => t.ErrorType);
+            descriptor.Field(t => t.Message);
+            descriptor.Field(t => t.Timestamp);
+            descriptor.Field(t => t.OriginatingMessage).Ignore();
+            base.Configure(descriptor);
+        }
+    }
+}
